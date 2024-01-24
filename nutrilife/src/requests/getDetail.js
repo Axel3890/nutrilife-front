13483af591344deda4e2488e4b787aaf791cf630
Axel Gomez <111620032+Axel3890@users.noreply.github.com>
2024-01-24 16:17:51 -0300

    Correcion de errores

--- nutrilife/src/requests/getDetail.js
+++ nutrilife/src/requests/getDetail.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getDetail = async (id) =>{
     try {
-        const data = await axios.get(`https://nutriback-fbb0k28kr-axel-gomezs-projects.vercel.app/${id}`)
+        const data = await axios.get(`https://nutriback-fbb0k28kr-axel-gomezs-projects.vercel.app/detail/${id}`)
         if(data.data.length === 0) return ("404")
         return data.data;
     } catch (error) {
--- nutrilife/src/requests/getRecipes.js
+++ nutrilife/src/requests/getRecipes.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getRecipes = async (query) =>{
     try {
-        const data = await axios.get(`https://nutriback-fbb0k28kr-axel-gomezs-projects.vercel.app/${query}`)
+        const data = await axios.get(`https://nutriback-fbb0k28kr-axel-gomezs-projects.vercel.app/recipes/${query}`)
         if(data.data.length === 0) return ("404")
         return data;
     } catch (error) {
